3a726c4713e020e303c4685d62c78a6cdf7daa0c
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-10 10:52:43 +0300

    Added Table View to Controllers

--- News/ViewController.swift
+++ News/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let List = tableView.dequeueReusableCell(withIdentifier: "itemList", for: indexPath)
+        return List
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
 }
 
