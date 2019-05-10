afb512b6d6ec0c2b5096c19b99478a060e22e80a
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-10 10:19:14 +0300

    Added Itemlist contoller class

---
+++ News/ItemList.swift
@@ -0,0 +1,30 @@
+//
+//  ItemList.swift
+//  News
+//
+//  Created by Joseph Njogu on 10/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class ItemList: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
