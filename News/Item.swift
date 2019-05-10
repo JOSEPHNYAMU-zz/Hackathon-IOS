88d3efbfbe655ab500c988bcfaa4123f1fe08557
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-10 10:36:44 +0300

    Added News Item Class

---
+++ News/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  News
+//
+//  Created by Joseph Njogu on 10/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    
+    var picture: String?
+    var title: String?
+    var body: String?
+    var author: String?
+    var path: String?
+
+}
---
+++ News/Items.swift
@@ -0,0 +1,15 @@
+//
+//  Items.swift
+//  News
+//
+//  Created by Joseph Njogu on 10/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class Items: NSObject {
+    
+    
+
+}
