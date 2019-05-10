00e27883d97ea47729a6f71e2a0b64c13bc4131c
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-10 14:35:52 +0300

    Added menu controller

---
+++ News/MenuController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuController.swift
+//  News
+//
+//  Created by Joseph Njogu on 10/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: NSObject {
+
+}
