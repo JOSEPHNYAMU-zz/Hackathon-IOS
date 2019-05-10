658363f5a3f9d423dc8bbc4fe05568fd5ea626a9
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-10 08:32:01 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Joseph Njogu on 10/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
