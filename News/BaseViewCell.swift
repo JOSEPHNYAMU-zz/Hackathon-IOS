6f00c36d80ba57a625b5ff04c7f800db260c4197
NYAMUJOSEPH <jnjogu@cytonn.com>
2019-05-12 22:47:56 +0300

    Added menu filter for sources

---
+++ News/BaseViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewCell.swift
+//  News
+//
+//  Created by Joseph Njogu on 12/05/2019.
+//  Copyright © 2019 Joseph Njogu. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- News/MenuController.swift
+++ News/MenuController.swift
@@ -12,6 +12,7 @@ class MenuController: NSObject, UITableViewDelegate, UITableViewDataSource {
     
     let menuView = UIView()
     let menuTableView = UITableView()
+    let arrayOfSources = ["ABC News", "Al Jazeera English", "Ary News", "Associated Press", "BBC Sport", "Business Insider"]
     
     public func showMenu() {
         
@@ -23,10 +24,18 @@ class MenuController: NSObject, UITableViewDelegate, UITableViewDataSource {
             
             menuView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissMenu)))
             
+            let height: CGFloat = 200
+            
+            let y = window.frame.height - height
+            
+            menuTableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             window.addSubview(menuView)
+            window.addSubview(menuTableView)
             
             UIView.animate(withDuration: 0.5, animations: {
                 self.menuView.alpha = 1
+                self.menuTableView.frame.origin.y = y
             })
             
         }
@@ -36,11 +45,40 @@ class MenuController: NSObject, UITableViewDelegate, UITableViewDataSource {
         
         UIView.animate(withDuration: 0.5, animations: {
             self.menuView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow {
+                self.menuTableView.frame.origin.y = window.frame.height
+            }
+            
         })
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arrayOfSources.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = arrayOfSources[indexPath.item]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 50
+    }
     
     override init() {
         super.init()
@@ -48,6 +86,7 @@ class MenuController: NSObject, UITableViewDelegate, UITableViewDataSource {
         menuTableView.dataSource = self
         menuTableView.isScrollEnabled = true
         menuTableView.bounces = true
+        menuTableView.register(BaseViewCell.classForCoder(), forCellReuseIdentifier: "cellId")
     }
 
 }
